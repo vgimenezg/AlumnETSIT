3834a7751cdeb36d45734820a06a8490cfe6d076
Rebeca Martínez <rebeca.martinez@interactiv4.com>
2017-02-11 12:29:01 +0100

    Improve Register success page code

--- imports/components/register/success.js
+++ imports/components/register/success.js
@@ -7,15 +7,28 @@ export default angular.module('registerSuccess', [
     angularMeteor
     ]).component('success', {
         templateUrl: 'imports/components/register/success.html',
-    }).controller('registerSuccessCtrl', ['$scope', function($scope) {
-        $scope.greeting = '¡Registro Completado!';
-        $scope.info = {
-            first: 'Podrás modificar tus datos en cualquier momento desde la sección Mi Perfil que encontrarás en el menú lateral.',
-            second: 'Te invitamos a que incluyas experiencias anteriores a tu puesto actual, con las que a cambio, conseguirás mas información de otros usuarios desde el Área de Intercambio'
-        };
-    }]).directive('info', function() {
+    }).controller('registerSuccessCtrl', RegisterSuccessCtrl
+    ).directive('info', function() {
         return {
             template: '<p>{{info.first}}</p><p>{{info.second}}</p>'
-        };
+        }
     });
 
+function RegisterSuccessCtrl($scope) {
+    $scope.greeting = {
+        text:'¡Registro Completado!',
+        icon:'check_circle'
+    };
+        
+    $scope.info = {
+            first: 'Podrás modificar tus datos en cualquier momento desde la sección "Mi Perfil" que encontrarás en el menú lateral.',
+            second: 'Te invitamos a que incluyas experiencias anteriores a tu puesto actual, con las que a cambio, conseguirás mas información de otros usuarios desde el Área de Intercambio'
+    };
+
+    $scope.button = {
+        options: [
+                {label: "Home", link: "/home", icon: "home"},
+                {label: "Añadir Experiencia", link: "/fullexperience", icon: "add"}
+        ],
+    };
+};
