ef30a76cce945ac39a8b307367bd44e2dbd1937e
Rebeca Martínez <rebeca.martinez@interactiv4.com>
2017-02-17 20:29:56 +0100

    Complete experience form

--- imports/components/experience/experience.js
+++ imports/components/experience/experience.js
@@ -3,14 +3,13 @@ import angularMeteor from 'angular-meteor';
 import template from './experience.html';
 import './experience.css'
 
-export default angular.module('experience', [
-  angularMeteor
-]).component('experience', {
-    templateUrl: 'imports/components/experience/experience.html',
+export default angular.module('experience', [angularMeteor])
+    .component('experience', { templateUrl: 'imports/components/experience/experience.html',
 })
     .controller("SwitchCtrl", SwitchCtrl)
     .controller("DatepickerCtrl",DatepickerCtrl)
-    .controller("TecnhologyCtrl",TecnhologyCtrl);
+    .controller("TecnhologyCtrl",TecnhologyCtrl)
+    .controller('ProfileCtrl', ProfileCtrl);
 
 
 function SwitchCtrl($scope) {
@@ -34,7 +33,7 @@ function DatepickerCtrl($scope) {
 
 function TecnhologyCtrl($scope, $element) {
     /*Sustituir por listado de tecnologias recogido de BBDD*/
-    $scope.technologies = ['Php' ,'JavaScript' ,'Java' ,'MongoDB' ,'MySql', 'Go'];
+    $scope.technologies = ['Php' ,'JavaScript' ,'Java' ,'MongoDB' ,'MySql', 'Go', 'HTML', 'Ada'];
     $scope.searchTerm;
     $scope.clearSearchTerm = function() {
         $scope.searchTerm = '';
@@ -43,3 +42,54 @@ function TecnhologyCtrl($scope, $element) {
           ev.stopPropagation();
     });
 }
+
+ function ProfileCtrl ($timeout, $q) {
+    var self = this;
+
+    // list of `profile` value/display objects
+    self.profiles      = loadAll();
+    self.selectedItem  = null;
+    self.searchText    = null;
+    self.querySearch   = querySearch;
+
+    // ******************************
+    // Internal methods
+    // ******************************
+
+    /**
+     * Search for profiles... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.profiles.filter( createFilterFor(query) ) : self.profiles;
+      var deferred = $q.defer();
+      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+      return deferred.promise;
+    }
+
+    /**
+     * Build `profiles` list of key/value pairs
+     */
+    function loadAll() {
+      var allProfiles = 'programador, analista programador, analista, Desarrollador Web, Consultor, técnico de sistemas, administrador, jefe de proyecto, soporte técnico, arquitecto TIC, redes, técnico software, helpdesk, técnico de bases de datos, tester, operador, seguridad, diseño, comercial, técnico hardware, técnico de mantenimiento, técnico de gestión, sistemas de calidad, I+D, administrativo, SEO/posicionamiento web, migración de datos, formador, auditor, reclutador';
+
+      return allProfiles.split(/, +/g).map( function (profile) {
+        return {
+          value: profile.toLowerCase(),
+          display: profile
+        };
+      });
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(profile) {
+        return (profile.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+  }
